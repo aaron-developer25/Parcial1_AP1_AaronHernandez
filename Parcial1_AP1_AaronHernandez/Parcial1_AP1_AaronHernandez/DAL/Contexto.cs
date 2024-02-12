3c7e756f140691845486f2724990a12572a230de
aaron-developer25 <atminifg655@gmail.com>
2024-02-12 18:40:29 -0400

    Creando el contexto

--- Parcial1_AP1_AaronHernandez/Parcial1_AP1_AaronHernandez/DAL/Contexto.cs
+++ Parcial1_AP1_AaronHernandez/Parcial1_AP1_AaronHernandez/DAL/Contexto.cs
@@ -4,8 +4,10 @@ using Parcial1_AP1_AaronHernandez.Models;
 
 namespace Parcial1_AP1_AaronHernandez.DAL
 {
-    public class Contexto
+    public class Contexto : DbContext
     {
+        public DbSet<Metas> Metas { get; set; }
 
+        public Contexto(DbContextOptions<Contexto> options) : base (options) { }
     }
 }
